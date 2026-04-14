e26e81101febcc1c7cd73f80f525e14f740b1bb0
josselin31 <J.brosseau31@outlook.fr>
2026-04-14 14:00:25 +0200

    miaouw

--- src/main/config/config.js
+++ src/main/config/config.js
@@ -1,25 +1,19 @@
 import path from 'path'
 import os from 'os'
 
-const isDev = process.env.NODE_ENV === 'development'
 const isWin = process.platform === 'win32'
+const winDataRoot = process.env.DUIX_DATA_ROOT_WIN || path.join('C:', 'duix_avatar_data')
+const unixDataRoot = path.join(os.homedir(), 'duix_avatar_data')
+const dataRoot = isWin ? winDataRoot : unixDataRoot
 
 export const serviceUrl = {
-  face2face: isDev ? 'http://192.168.4.204:8383/easy' : 'http://127.0.0.1:8383/easy',
-  tts: isDev ? 'http://192.168.4.204:18180' : 'http://127.0.0.1:18180'
+  face2face: process.env.DUIX_FACE2FACE_BASE_URL || 'http://127.0.0.1:8383/easy',
+  tts: process.env.DUIX_TTS_BASE_URL || 'http://127.0.0.1:18180'
 }
 
 export const assetPath = {
-  model: isWin
-    ? path.join('D:', 'duix_avatar_data', 'face2face', 'temp')
-    : path.join(os.homedir(), 'duix_avatar_data', 'face2face', 'temp'), // 模特视频
-  ttsProduct: isWin
-    ? path.join('D:', 'duix_avatar_data', 'face2face', 'temp')
-    : path.join(os.homedir(), 'duix_avatar_data', 'face2face', 'temp'), // TTS 产物
-  ttsRoot: isWin
-    ? path.join('D:', 'duix_avatar_data', 'voice', 'data')
-    : path.join(os.homedir(), 'duix_avatar_data', 'voice', 'data'), // TTS服务根目录
-  ttsTrain: isWin
-    ? path.join('D:', 'duix_avatar_data', 'voice', 'data', 'origin_audio')
-    : path.join(os.homedir(), 'duix_avatar_data', 'voice', 'data', 'origin_audio') // TTS 训练产物
+  model: path.join(dataRoot, 'face2face', 'temp'), // 模特视频
+  ttsProduct: path.join(dataRoot, 'face2face', 'temp'), // TTS 产物
+  ttsRoot: path.join(dataRoot, 'voice', 'data'), // TTS服务根目录
+  ttsTrain: path.join(dataRoot, 'voice', 'data', 'origin_audio') // TTS 训练产物
 }
